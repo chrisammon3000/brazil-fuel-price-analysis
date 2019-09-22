ee986bd0382ba1cd807551824beda0e4d4d56311
Gregory Lindsey <gclindsey@gmail.com>
2019-09-21 21:44:11 -0700

    Delete code, Add function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,4 @@
 # Read data function
 def read_data(raw_data):
 
-    data = pd.read_csv(raw_data, sep='\t',parse_dates=True)
-
-    return data
+    return pd.read_csv(raw_data, sep='\t',parse_dates=True)
